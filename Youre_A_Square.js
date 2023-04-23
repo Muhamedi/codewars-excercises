b8f85b0f0d07dbe2c1a9861a75da28d0e0ca2280
Muhamedi <muhamed.r.krasniqi@gmail.com>
2023-04-23 22:08:17 +0200

    Update Youre_A_Square.js

--- Youre_A_Square.js
+++ Youre_A_Square.js
@@ -3,11 +3,13 @@
    Date solved - 09/01/2023
    
  - Problem description
-   The goal of this exercise is to convert a string to a new string where each character in the new string
-   is "(" if that character appears only once in the original string, or ")" if that character appears more 
-   than once in the original string. Ignore capitalization when determining if a character is a duplicate. 
+   Given an integral number, determine if it's a square number:. 
+   
  - Solution 1
- - Time Complexity O(n)
- - Explanation: Iterate and keep count of each character occurrence and then iterate and based
-   on character occurrence frequency construct the string with the correct parentheses
+ - Time Complexity O(1)
+ - Explanation: Calculate the square root of the number and check if its an integer
  */
+
+const isSquare = (n) => {
+  return Number.isInteger(Math.sqrt(n))
+}
