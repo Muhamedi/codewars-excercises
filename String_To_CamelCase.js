18f4536596dff049ef5ab35f67699d07d7e9e727
Muhamedi <muhamed.r.krasniqi@gmail.com>
2023-04-23 22:20:56 +0200

    Update String_To_CamelCase.js

--- String_To_CamelCase.js
+++ String_To_CamelCase.js
@@ -1,3 +1,20 @@
+/* 
+   Language - Javascript
+   Date solved - 07/01/2023
+
+ - Problem description
+   Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
+   The first word within the output should be capitalized only if the original word was capitalized 
+   (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized. 
+
+ - Solution 1
+ - Time Complexity O(n) where n is the length of the input string
+ - Explanation: Split the string by underscore or dash, then loop through each word
+   Convert to uppercase the first character of all words except the first so 
+   that it can keep the original first letter casing as required by the problem
+   Save the transformed words back and join them
+ */
+
 function toCamelCase(str){
   if(!str) {
     return str;
