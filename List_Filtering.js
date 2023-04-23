34ae6785030a67dcf5d031e1c6b542687b0ace11
Muhamedi <muhamed.r.krasniqi@gmail.com>
2023-04-23 22:01:20 +0200

    Update List_Filtering.js

--- List_Filtering.js
+++ List_Filtering.js
@@ -1,14 +1,16 @@
 
-# Language - Javascript
-# Date solved - 10/01/2023
+/* 
+   Language - Javascript
+   Date solved - 10/01/2023
 
-# - Problem description
-#   In this kata you will create a function that takes a list of non-negative integers 
-#   and strings and returns a new list with the strings filtered out. 
+ - Problem description
+   In this kata you will create a function that takes a list of non-negative integers 
+   and strings and returns a new list with the strings filtered out. 
 
-# - Solution 1
-# - Time Complexity O(n)
-# - Explanation: Check the type of each element and filter out those of type string
+ - Solution 1
+ - Time Complexity O(n)
+ - Explanation: Check the type of each element and filter out those of type string 
+ */
 
 function filter_list(l) {
   return l.filter(x => typeof x !== 'string');
